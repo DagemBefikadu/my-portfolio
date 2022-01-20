a2d7c3eca12e5ff27b970d889dd758cb1ba411a8
DagemBefikadu <befikadu.dagem@gmail.com>
2022-01-20 14:26:02 -0800

    added logo image

--- src/partials/Header.js
+++ src/partials/Header.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import {Nav, Navbar, Container, Image} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import logo from '../Static/DagemLogo.png'
+
+
 
 export default function Header() {
+    const backgroundStyle = {
+        backgroundColor: '#7c6c89'
+    }    
     return (
         <div>
             <Navbar variant='dark' expand='md'>
                     <Navbar.Brand>
-                        <Image src="public/DagemLogo.png"/>
+                        <Image src={logo} width="200" height="200"/>
                     </Navbar.Brand>
                     <Nav.Link></Nav.Link>
                     <Nav.Link>About</Nav.Link>
