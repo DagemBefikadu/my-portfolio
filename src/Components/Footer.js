dde1a11bdc25d88bf27c4acf497e597860dcdce2
DagemBefikadu <befikadu.dagem@gmail.com>
2022-02-15 19:22:11 -0800

    Spelling error

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <div>
             <h6>Dagem Befikadu - 2022 - REACT</h6>
-            <h6>Contaact info </h6>
+            <h6>Contact info </h6>
         </div>
     )
 }
