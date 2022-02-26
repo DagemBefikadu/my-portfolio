5c18c1a8d91a5c368c214fc751b713dccf954e83
DagemBefikadu <befikadu.dagem@gmail.com>
2022-02-25 23:25:41 -0800

    cleaning up code

--- src/partials/Header.js
+++ src/partials/Header.js
@@ -2,9 +2,8 @@ import React from "react";
 import Image from "react-bootstrap/Image";
 import { Link } from "react-router-dom";
 import logo from "../Static/DagemLogo.png";
-import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import Container from "react-bootstrap/Container";
+
 
 
 export default function Header() {
