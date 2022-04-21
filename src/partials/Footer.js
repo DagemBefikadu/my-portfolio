40a0e3d7e296734085a0b75778873499da596c90
DagemBefikadu <befikadu.dagem@gmail.com>
2022-04-20 22:14:11 -0700

    adding footer

--- src/partials/Footer.js
+++ src/partials/Footer.js
@@ -26,6 +26,7 @@ export default function Header() {
       <Navbar.Brand>
         <Image src={logo} width="300" height="300" />
       </Navbar.Brand>
+      {/* Change the color */}
     </>
   );
 }
